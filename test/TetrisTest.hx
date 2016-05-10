40cac5bf76dfe11be33fcc00ce0b26ede58f6f9d
Heorhiy Kharvat <egor.harvat@dubitlimited.com>
2016-05-11 00:21:36 +0300

    Placeholdered test added

---
+++ test/TetrisTest.hx
@@ -0,0 +1,37 @@
+package;
+
+import massive.munit.Assert;
+
+class TetrisTest
+{
+	public function new()
+	{
+	}
+
+	@BeforeClass
+	public function beforeClass():Void
+	{
+	}
+
+	@AfterClass
+	public function afterClass():Void
+	{
+	}
+
+	@Before
+	public function setup():Void
+	{
+	}
+
+	@After
+	public function tearDown():Void
+	{
+	}
+
+	//Just a placeholder for now
+	@Test
+	public function testExample():Void
+	{
+		Assert.isTrue(true);
+	}
+}
