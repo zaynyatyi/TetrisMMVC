93be2c87c78acaae9e160d30581412243f881bd3
Heorhiy Kharvat <egor.harvat@dubitlimited.com>
2016-05-11 08:01:36 +0300

    Not implemented comment added

--- src/tetris/models/ElementsModel.hx
+++ src/tetris/models/ElementsModel.hx
@@ -5,6 +5,7 @@ import tetris.models.ElementModel;
 class ElementsModel
 {
 	public var currentChunk:ElementModel;
+	//That chunk supposed to be used as next element preview (Not yet implemented)
 	public var nextChunk:ElementModel;
 
 	public function new()
